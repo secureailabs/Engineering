b76ab0f1ff5c33eff6df98cbf488bd206afb176c
David-G-SAIL <david@secureailabs.com>
2022-01-28 13:49:08 -0500

    BOARD-1192 Remove commented out code
    Removing code from legacy orchestrator

--- Milestone5/VirtualMachine/Orchestrator/sail/sail/core/__init__.py
+++ Milestone5/VirtualMachine/Orchestrator/sail/sail/core/__init__.py
@@ -1,7 +1,4 @@
-from .sailcore import connect, login, newguid, get_fns, pushdata, pulldata,\
-     pushsafeobj, submitjob, registersafeobj, queryresult, queryresults_parallel,\
-     queryjobstatus, quit, setparameter, querydata, create_dummy_data, dataInfo,\
-     VMSetup, get_safe_functions, load_safe_objects,\
+from .sailcore import login, get_safe_functions, load_safe_objects,\
      get_current_eosb, exit_current_session, get_digital_contracts, get_datasets, get_tables,\
      provision_secure_computational_node, run_job, wait_for_all_secure_nodes_to_be_provisioned,\
      set_parameter, push_user_data, get_job_status, pull_data, wait_for_data

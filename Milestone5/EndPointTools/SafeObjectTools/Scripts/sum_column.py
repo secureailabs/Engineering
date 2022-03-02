3c4e43d62c39b93609205e8e23e46c11b04672a3
David-G-SAIL <david@secureailabs.com>
2022-03-02 08:54:33 -0500

    Add missing safe object script

--- Milestone5/EndPointTools/SafeObjectTools/Scripts/sum_column.py
+++ Milestone5/EndPointTools/SafeObjectTools/Scripts/sum_column.py
@@ -1,3 +1,7 @@
+import pandas as pd
 
-def Run(value: int, dummy_ds: int) -> (int, [True]):
-    return value + 1
+
+def Run(df: pd.DataFrame,
+        col: str,
+        dummy_ds: str) -> (int, [True]):
+    return int(df[col].sum())

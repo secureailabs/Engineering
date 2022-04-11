28dd4bfe690959949b22db5490c2c759b31818c2
Prawal Gangwar <prawal.gangwar@gmail.com>
2022-04-11 19:11:27 +0530

    [BOARD-1420] dataservices + bootstrap nginx

--- Docker/dataservices/Entrypoint.sh
+++ Docker/dataservices/Entrypoint.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+set -e
+imageName=dataservices
+
+cd /app || exit
+
 # Start the nginx server
 nginx -g 'daemon off;' 2>&1 | tee /app/nginx.log &
 
-cd /app || exit
+# BaseVmImageInit will download the package.tar.gz and InitializationVector.json
+# if they are not already present on the file system.
+./BaseVmImageInit
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    exit $retVal
+fi
+
+# Unpack the tar package
+tar -xvf package.tar.gz
+
+# Move the InitializerVector to the Binary folder
+mv InitializationVector.json Binary/$imageName/
+
+# Start the RestApiPortal
+cd Binary/$imageName
 mongod --port 27017 --dbpath /srv/mongodb/db0 --replSet rs0 --bind_ip localhost --fork --logpath /var/log/mongod.log
 ./DatabaseGateway 2>&1 | tee databasegateway.log &
 tail -f /dev/null

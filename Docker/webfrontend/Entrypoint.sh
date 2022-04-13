7b3d06fa582ec4f51b008a12abfa415e1a8bd1df
ADAM JASNIEWICZ <adam@secureailabs.com>
2022-04-13 09:26:03 -0400

    Switched to React Query

--- Docker/webfrontend/Entrypoint.sh
+++ Docker/webfrontend/Entrypoint.sh
@@ -21,5 +21,4 @@ popd
 yarn --cwd client install && yarn cache clean && yarn --cwd client build
 yarn --cwd server install && yarn cache clean
 
-chmod +x /app/env.sh
-/app/env.sh && yarn --cwd /app/server prod && /bin/bash
+yarn --cwd /app/server prod && /bin/bash

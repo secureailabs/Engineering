a6a3780b3a6703004ba8c3714e75908b0dfb234a
Vincent Quirion <vincent@secureailabs.com>
2022-05-02 15:57:48 -0400

    fixed digital contracts tab filter

--- WebFrontend/client/src/pages/DigitalContracts/DigitalContracts/DigitalContracts.success.tsx
+++ WebFrontend/client/src/pages/DigitalContracts/DigitalContracts/DigitalContracts.success.tsx
@@ -74,8 +74,9 @@ const DigitalContractsSuccess: React.FC<TDigitalContractsSuccessProps> = ({
   );
   const [current_filter, setCurrentFilter] = useState('1');
   const count = (stage: 1 | 2 | 3 | 4): number => {
+    console.log(Object.entries(getAllDigitalContractsData))
     return Object.entries(getAllDigitalContractsData).filter(
-      ([key, value]) => value.ContractStage == stage
+      ([key, value]) => getDCStage(value.ContractStage, value.ExpirationTime) == stage
     ).length;
   };
 

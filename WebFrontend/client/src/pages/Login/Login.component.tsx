01e3dd54f03808723815cbb2ae4599240d04aff3
Vincent Quirion <vincent@secureailabs.com>
2022-05-25 12:04:35 -0400

    fixed spinner

--- WebFrontend/client/src/pages/Login/Login.component.tsx
+++ WebFrontend/client/src/pages/Login/Login.component.tsx
@@ -6,6 +6,7 @@ import { TLoginProps } from './Login.types';
 import LoginForm from './Login.form';
 import LoginSuccess from './Login.success';
 import LoginFailure from './Login.failure';
+import { AbsoluteSpinner } from '@components/Spinner';
 
 const Login: React.FC<TLoginProps> = ({
   signInReset,
@@ -17,6 +18,7 @@ const Login: React.FC<TLoginProps> = ({
       status={status}
       success={LoginSuccess}
       failure={() => <LoginFailure signInReset={signInReset} />}
+      Loading={<AbsoluteSpinner />}
     >
       <LoginForm signInReset={signInReset} signInStart={signInStart} />
     </ConditionalRender>

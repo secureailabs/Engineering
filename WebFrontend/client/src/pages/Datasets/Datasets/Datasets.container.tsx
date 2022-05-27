e0a28d8408ec32d75d221454d7b765d9cb46aa01
Vincent Quirion <vincent@secureailabs.com>
2022-05-27 14:51:52 -0400

    test change

--- WebFrontend/client/src/pages/Datasets/Datasets/Datasets.container.tsx
+++ WebFrontend/client/src/pages/Datasets/Datasets/Datasets.container.tsx
@@ -4,6 +4,8 @@ import { AxiosError } from 'axios';
 import { TGetAllDatasetsSuccess } from '@APIs/dataset/dataset.typeDefs';
 import { getAllDatasetsAPI } from '@APIs/dataset/dataset.apis';
 
+import demo_data from '@APIs/dataset/dataset.data';
+
 import Datasets from './Datasets.component';
 
 const DatasetsContainer: React.FC = () => {
@@ -11,7 +13,8 @@ const DatasetsContainer: React.FC = () => {
     // @ts-ignore
     useQuery<TGetAllDatasetsSuccess['datasets'], AxiosError>(['datasets'], getAllDatasetsAPI, { refetchOnMount: 'always' });
   //@ts-ignore
-  return Datasets({ status: status, getAllDatasetsData: data, refetch: refetch, error: error})
+  return localStorage.getItem('mode') == 'demo' ? Datasets({ status: 'success', getAllDatasetsData: demo_data, error: null }) : Datasets({ status: status, getAllDatasetsData: data, error: error })
+  
 }
 
 export default DatasetsContainer;

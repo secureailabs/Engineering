5d32f324e66d8e1b961a69511d16a9164e432208
Stanley <stanley@secureailabs.com>
2022-06-13 17:02:07 -0400

    cleanup of docker scripts

--- Docker/BuildImages.sh
+++ Docker/BuildImages.sh
@@ -26,6 +26,13 @@ while getopts "i:" opt; do
     esac
 done
 
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
+# Prune unused docker networks
+docker network prune -f
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
+
 # Create sailNetwork if it does not exist
 networkName="sailNetwork"
 foundNetworkName=$(docker network ls --filter name=$networkName --format {{.Name}})
@@ -36,6 +43,10 @@ else
     echo "Creating network"
     docker network create --subnet=172.31.252.0/24 $networkName
 fi
+echo "--------------------------------------------------"
+docker network ls
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
 
 # Build the asked image if it specified in the input parameters
 if [ -z "$imageName" ]; then

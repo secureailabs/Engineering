aa3063182a5026401f6e4c792197a1fef1cf2c8d
Prawal Gangwar <prawal.gangwar@gmail.com>
2022-06-14 22:36:35 -0400

    [BOARD-1850] cleanup

--- Make/Modules.mk
+++ Make/Modules.mk
@@ -4,7 +4,6 @@ ROOTDIR?=$(shell realpath .)
 DATASET_VIEWER_TOOL:=$(ROOTDIR)/InternalTools/DatasetViewer
 DATABASE_INITIALIZATION_TOOL:=$(ROOTDIR)/InternalTools/DatabaseInitializationTool
 SAFE_OBJECT_COMPILER:=$(ROOTDIR)/EndPointTools/SafeObjectTools/SafeObjectCompiler/Source
-UPLOAD_TOOL:=$(ROOTDIR)/InternalTools/UploadPackageAndInitializationVector
 CRYPTOGRAPHIC_TOOLS:=$(ROOTDIR)/InternalTools/CryptographicTools
 REMOTE_DATA_CONNECTOR:=$(ROOTDIR)/InternalTools/RemoteDataConnector
 

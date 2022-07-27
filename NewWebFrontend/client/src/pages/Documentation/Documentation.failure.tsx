2a6e2590162683a28ce205593c68312488a3f255
Prawal Gangwar <prawal.gangwar@gmail.com>
2022-07-27 15:20:14 -0400

    review typo fix

--- NewWebFrontend/client/src/pages/Documentation/Documentation.failure.tsx
+++ NewWebFrontend/client/src/pages/Documentation/Documentation.failure.tsx
@@ -2,7 +2,7 @@ import { TDocumentationFailureProps } from "./Documentation.types";
 
 const DocumentationFailure : React.FC<TDocumentationFailureProps> = () => {
   return (
-    <p>There was an error fetching list of documentation. Please try again later...</p>
+    <p>There was an error fetching the list of documentation pages. Please try again later...</p>
   );
 };
 

4fc67c209ce21b26b893478cd99e327871495dd6
Stanley <stanley@secureailabs.com>
2022-09-15 15:08:38 -0400

    BOARD-1746, CI to publish Docs

--- ApiServices/generators.sh
+++ ApiServices/generators.sh
@@ -2,7 +2,7 @@
 rm -f docs/openapi.json
 
 # Download the API spec
-wget http://127.0.0.1:8000/openapi.json -P docs/
+wget https://127.0.0.1:8000/openapi.json -P docs/ --no-check-certificate
 
 # Generate API documentation
 redoc-cli bundle -o docs/index.html docs/openapi.json

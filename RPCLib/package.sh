f8cde0cbc7ec9e60e4a6c8dac3cd09066a4d30e7
JingweiZhangBoston <kukuthedoydoy@gmail.com>
2022-09-23 12:32:10 -0400

    change according to comments

--- RPCLib/package.sh
+++ RPCLib/package.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#package necessary files and dirs
 tar -zcvf package.tar.gz series.py test_server.py dataframe.py custom_typing.py private_keys public_keys zero
-mv ./package.tar.gz ../Docker/rpcrelated/
+
+#move the package to Binary
+mv ./package.tar.gz ../Binary/rpcrelated_dir/

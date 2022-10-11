0195d716fda7c7816a5963eb3bd2f63a970cc8c7
JingweiZhangBoston <kukuthedoydoy@gmail.com>
2022-10-11 11:37:06 -0400

    BOARD-1986 change according to comments

--- RPCLib/zero/zero/__init__.py
+++ RPCLib/zero/zero/__init__.py
@@ -17,6 +17,3 @@ from zero.client import AsyncZeroClient, ZeroClient
 from zero.customtypes import ProxyObject, SecretObject
 from zero.serialize import deserializer_table, serializer_table
 from zero.server import ZeroServer, load_module
-
-# no support for now -
-# from .logger import AsyncLogger

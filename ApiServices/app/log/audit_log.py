018c8d2d7cdb6a124c736d581df79212a6150496
JingweiZhangBoston <kukuthedoydoy@gmail.com>
2022-11-07 09:33:59 -0500

    BOARD-1990 update according to comments

--- ApiServices/app/log/audit_log.py
+++ ApiServices/app/log/audit_log.py
@@ -1,3 +1,18 @@
+# -------------------------------------------------------------------------------
+# Engineering
+# audit_log.py
+# -------------------------------------------------------------------------------
+
+# """Module to write audit logs"""
+# -------------------------------------------------------------------------------
+# Copyright (C) 2022 Secure Ai Labs, Inc. All Rights Reserved.
+# Private and Confidential. Internal Use Only.
+# This software contains proprietary information which shall not
+# be reproduced or transferred to other documents and shall not
+# be disclosed to others for any purpose without
+# prior written permission of Secure Ai Labs, Inc.
+# -------------------------------------------------------------------------------
+
 import logging
 import os
 import sys

cc621770928df4d3776266a397c90e8772539a01
JingweiZhangBoston <kukuthedoydoy@gmail.com>
2022-11-16 18:31:25 -0500

    BOARD 1979 BOARD 1980 fixes according to comments

--- Docker/apiservices/Entrypoint.sh
+++ Docker/apiservices/Entrypoint.sh
@@ -19,11 +19,19 @@ fi
 # Unpack the tar package
 tar -xf package.tar.gz
 
+# Use the InitializationVector to populate the IP address of the audit services
+auditIP=$(cat InitializationVector.json | jq -r '.audit_service_ip')
+auditPort=$(cat InitializationVector.json | jq -r '.audit_service_port')
+
 # Move the InitializerVector to the Binary folder
 mv InitializationVector.json ApiServices/
 
 # Start the local mongodb database
 mongod --port 27017 --dbpath /srv/mongodb/db0 --bind_ip localhost --fork --logpath /var/log/mongod.log
+
+# modify the audit service ip of promtail config file
+sed -i "s,auditserver,$auditIP,g" /promtail-local-config.yaml
+sed -i "s,3100,$auditPort,g" /promtail-local-config.yaml
 
 # Start the promtail client
 /promtail-linux-amd64 -config.file=/promtail-local-config.yaml  > /promtail.log 2>&1&

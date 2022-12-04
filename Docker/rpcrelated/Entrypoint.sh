9092bd8ae00d5198c99494e1be570baad2a1bcc9
Prawal Gangwar <prawal.gangwar@gmail.com>
2022-12-04 20:27:57 +0530

    azure deployment

--- Docker/rpcrelated/Entrypoint.sh
+++ Docker/rpcrelated/Entrypoint.sh
@@ -29,7 +29,13 @@ datasetVersionId=$(cat InitializationVector.json | jq -r '.dataset_version_id')
 datasetId=$(cat InitializationVector.json | jq -r '.dataset_id')
 
 localDataset=$(cat InitializationVector.json | jq -r '.local_dataset')
-mkdir -p /mnt/dataset
+if [ "$localDataset" = "null" ]; then
+    localDataset=false
+fi
+
+mountDir="/mnt/azure"
+mkdir -p $mountDir
+
 if $localDataset; then
     echo "Using local dataset: $datasetVersionId"
     ls /
@@ -41,6 +47,12 @@ else
         mkdir /etc/smbcredentials
     fi
 
+    if [ ! -f "/etc/smbcredentials/$storageAccountName.cred" ]; then
+        echo "username=$storageAccountName" >> /etc/smbcredentials/$storageAccountName.cred
+        echo "password=$datasetStoragePassword" >> /etc/smbcredentials/$storageAccountName.cred
+    fi
+
+    chmod 600 /etc/smbcredentials/$storageAccountName.cred
     echo "//$storageAccountName.file.core.windows.net/$datasetId/$datasetVersionId $mountDir cifs nofail,credentials=/etc/smbcredentials/$storageAccountName.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab
     mount -t cifs //$storageAccountName.file.core.windows.net/$datasetId/$datasetVersionId $mountDir -o credentials=/etc/smbcredentials/$storageAccountName.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
 
@@ -58,6 +70,7 @@ else
     # Decrypt the dataset_content file
     aesTag=$(cat /data/dataset_header.json | jq -r '.aes_tag')
     aesNonce=$(cat /data/dataset_header.json | jq -r '.aes_nonce')
+    datasetKey=$(cat InitializationVector.json | jq -r '.dataset_key')
     python3 decrypt_file.py -i /data/data_content.zip -o /data/data_content.zip -k $datasetKey -n $aesNonce -t $aesTag
 
     # Unzip the dataset_content zip file
@@ -65,7 +78,6 @@ else
     unzip /data/data_content.zip -d /data/data_content
     rm -rf /data/data_content.zip
 fi
-
 
 # Install the rpc library
 pip3 install /app/zero
@@ -75,8 +87,7 @@ pip3 install /ds/sail-safe-functions-orchestrator
 pip3 install /ds/helper-libs
 
 # Start the rpc server
-PATH_DIR_PUBLIC_KEY_ZEROMQ=/app/public_keys/ PATH_FILE_PRIVATE_KEY_ZEROMQ_SERVER=/app/private_keys/server.key_secret PATH_DIR_DATASET=/data/ python3 /ds/sail-safe-functions-test/integration_test/test_server.py 5001
+PATH_DIR_PUBLIC_KEY_ZEROMQ=/app/public_keys/ PATH_FILE_PRIVATE_KEY_ZEROMQ_SERVER=/app/private_keys/server.key_secret PATH_DIR_DATASET=/data/ python3 /ds/sail-safe-functions-test/integration_test/test_server.py 5556
 
 # To keep the container running
 tail -f /dev/null
-

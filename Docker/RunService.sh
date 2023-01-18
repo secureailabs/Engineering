9f73e327ca8d985188801779e6d49ead0507bdd0
Prawal Gangwar <prawal.gangwar@gmail.com>
2023-01-18 17:56:58 +0530

    CI fixes

--- Docker/RunService.sh
+++ Docker/RunService.sh
@@ -97,20 +97,8 @@ if [ "apiservices" == "$imageName" ]; then
         docker volume create $sailDatabaseVolumeName
     fi
     make -C $rootDir package_apiservices -s
-    # Check for Azure environment variables
-    if [ -z "${AZURE_SUBSCRIPTION_ID}" ]; then
-        echo "environment variable AZURE_SUBSCRIPTION_ID is undefined. Using development as default."
-        AZURE_SUBSCRIPTION_ID="b7a46052-b7b1-433e-9147-56efbfe28ac5"
-    fi
-    if [ ${AZURE_SUBSCRIPTION_ID} == "3d2b9951-a0c8-4dc3-8114-2776b047b15c" ]; then
-        cp apiservices/InitializationVectorScratchPad.json.bak $rootDir/Binary/apiservices_dir/InitializationVector.json
-    elif  [ ${AZURE_SUBSCRIPTION_ID} == "b7a46052-b7b1-433e-9147-56efbfe28ac5" ]; then
-        cp apiservices/InitializationVectorDevelopment.json.bak $rootDir/Binary/apiservices_dir/InitializationVector.json
-    elif  [${AZURE_SUBSCRIPTION_ID} == "40cdb551-8a8d-401f-b884-db1599022002" ]; then
-        cp apiservices/InitializationVectorReleaseCandidate.json.bak $rootDir/Binary/apiservices_dir/InitializationVector.json
-    elif  [ ${AZURE_SUBSCRIPTION_ID} == "ba383264-b9d6-4dba-b71f-58b3755382d8" ]; then
-        cp apiservices/InitializationVectorProductionGA.json.bak $rootDir/Binary/apiservices_dir/InitializationVector.json
-    fi
+    cp apiservices/InitializationVector.json $rootDir/Binary/apiservices_dir
+    cat $rootDir/Binary/apiservices_dir/InitializationVector.json
     cp $rootDir/Binary/apiservices.tar.gz $rootDir/Binary/apiservices_dir/package.tar.gz
     runtimeFlags="$runtimeFlags -p 8000:8001 -p 9080:9080 -v $sailDatabaseVolumeName:/srv/mongodb/db0 -v $rootDir/Binary/apiservices_dir:/app $imageName"
 elif [ "newwebfrontend" == "$imageName" ]; then

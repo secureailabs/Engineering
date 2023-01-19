175d90eecf0116461fde3327e2ca6dbacb536f42
Prawal Gangwar <prawal.gangwar@gmail.com>
2023-01-19 15:21:11 +0530

    [BOARD-2187] remove unused code

--- Make/Modules.mk
+++ Make/Modules.mk
@@ -1,6 +1,4 @@
 # Define ROOTDIR only if not already defined
 ROOTDIR?=$(shell realpath .)
 
-DATABASE_INITIALIZATION_TOOL:=$(ROOTDIR)/DatabaseInitializationTool
 VM_INITIALIZER:=$(ROOTDIR)/VmInitializer
-SHARED_COMMON_CODE:=$(ROOTDIR)/SharedCommonCode
